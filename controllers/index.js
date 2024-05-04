1a2d8019186badfd77723e30c448c980248cbe64
ashjoy7 <ashjoy7@gmail.com>
2024-05-04 12:31:46 -0700

    update

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-const awesomeFunction = (req, res, next) => {
-    res.json('Angela Egan');
-};
-
-const returnanotherPerson = (req, res, next) => {
-    res.json('Mark Egan');
-};
-
-module.exports = {awesomeFunction, returnanotherPerson};
