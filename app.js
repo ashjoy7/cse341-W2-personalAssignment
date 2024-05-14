c5ff02ec8729dcd9e817eff86bf5116efd0ef983
ashjoy7 <ashjoy7@gmail.com>
2024-05-14 12:00:49 -0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-const swaggerRoutes = require('./routes/swagger'); // Import the Swagger routes
+const swaggerRoutes = require('./routes/swagger'); 
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -14,7 +14,14 @@ app
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-  })
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    next();
+    })
   .use('/', require('./routes'));
 
 // Serve Swagger UI
